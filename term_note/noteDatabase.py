c943630daf19d0b09b4ebde43a3612f2f3b56718
phil0s0pher <c.lambert1213@gmail.com>
2017-11-24 14:26:54 -0800

    chore(env): Clean git env

--- term_note/noteDatabase.py
+++ term_note/noteDatabase.py
@@ -1,14 +1,13 @@
-import sqlite3
 import datetime
 
 
 class NoteDatabase:
     def __init__(self, conn):
-        self.db = conn
+        self.db_con = conn
 
-    def create_tables(self, db):
-        with db:
-            db.execute("""
+    def create_tables(self):
+        with self.db_con as con:
+            con.execute("""
             CREATE TABLE IF NOT EXISTS note
             (note_id INTEGER PRIMARY KEY,
             title TEXT,
@@ -16,12 +15,12 @@ class NoteDatabase:
             date_created TEXT,
             date_modified TEXT)""")
 
-            db.execute("""
+            con.execute("""
             CREATE TABLE IF NOT EXISTS tag
             (tag_id INTEGER PRIMARY KEY,
             name TEXT)""")
 
-            db.execute("""
+            con.execute("""
             CREATE TABLE IF NOT EXISTS tagmap
             (tagmap_id INTEGER PRIMARY KEY,
             note_id INTEGER,
@@ -29,34 +28,36 @@ class NoteDatabase:
             FOREIGN KEY(note_id) REFERENCES note(id),
             FOREIGN KEY(tag_id) REFERENCES tag(id))""")
 
-    def add_note(self, conn, title, text, tags=[]):
+    def add_tags(self, note_id, tags):
+        for tag in tags:
+            tag_id = self.db_con.execute(
+                "SELECT tag_id FROM tag WHERE name = ?", (tag, )).fetchone()
+            if (not tag_id):
+                with self.db_con as con:
+                    tag_id = con.execute("INSERT INTO tag (name) VALUES(?)",
+                                         (tag, )).lastrowid
+            else:
+                tag_id = tag_id[0]
+
+            with self.db_con as con:
+                con.execute("INSERT INTO tagmap (note_id, tag_id) VALUES(?,?)",
+                            (note_id, tag_id))
+
+    def add_note(self, title, text, tags=[]):
         date = datetime.datetime.now()
-        with conn:
-            note_id = conn.execute(
+        with self.db_con as con:
+            note_id = con.execute(
                 "INSERT INTO note (title, text, date_created, date_modified) VALUES(?,?,?,?)",
                 (title, text, date, date)).lastrowid
 
-            for tag in tags:
-                tag_id = conn.execute("SELECT tag_id FROM tag WHERE name = ?",
-                                      (tag, )).fetchone()
-                if (not tag_id):
-                    with conn:
-                        tag_id = conn.execute(
-                            "INSERT INTO tag (name) VALUES(?)",
-                            (tag, )).lastrowid
-                else:
-                    tag_id = tag_id[0]
+            self.add_tags(note_id, tags)
 
-                with conn:
-                    conn.execute(
-                        "INSERT INTO tagmap (note_id, tag_id) VALUES(?,?)",
-                        (note_id, tag_id))
         return note_id
 
-    def delete_note(self, conn, id):
-        with conn:
-            conn.execute("DELETE FROM note WHERE note_id = ? ", (id, ))
-            conn.execute("DELETE FROM tagmap WHERE note_id = ?", (id, ))
+    def delete_note(self, id):
+        with self.db_con as con:
+            con.execute("DELETE FROM note WHERE note_id = ? ", (id, ))
+            con.execute("DELETE FROM tagmap WHERE note_id = ?", (id, ))
 
-    def update_note(self, conn, id, title, text, tags):
+    def update_note(self, id, title, text, tags):
         pass
--- term_note/tests/test_noteDatabase.py
+++ term_note/tests/test_noteDatabase.py
@@ -19,59 +19,50 @@ def note_db(conn):
 
 
 def test_create_tables(conn, note_db):
-    note_db.create_tables(conn)
+    note_db.create_tables()
     assert (conn.execute("SELECT name FROM sqlite_master WHERE type='table';")
             .fetchall() == [("note", ), ("tag", ), ("tagmap", )])
 
 
 def test_add_note_with_tags(conn, note_db):
-    note_id = note_db.add_note(conn, "stuff", "things", ["tags", "moreTags"])
-    assert (conn.execute("SELECT note_id FROM note WHERE note_id= ?;", (note_id, ))
-            .fetchone() == (note_id, ))
-    assert (conn.execute("SELECT name FROM tag").fetchall() == [
-        ("tags", ), ("moreTags", )
-    ])
+    note_id = note_db.add_note("stuff", "things", ["tags", "moreTags"])
 
-    tag_ids = map(lambda x: x[0],
-                  conn.execute("SELECT tag_id FROM tag").fetchall())
-    tagmap = list(map(lambda x: (note_id, x), tag_ids))
-    assert (conn.execute(
-        "SELECT note_id, tag_id FROM tagmap WHERE note_id = ?",
-        (note_id, )).fetchall() == tagmap)
+    assert (conn.execute("""SELECT note.note_id, tag.name FROM note
+        JOIN (tagmap JOIN tag ON tagmap.tag_id = tag.tag_id)
+        ON note.note_id = tagmap.note_id
+        WHERE note.note_id = ?""",
+                         (note_id, )).fetchall() == [(note_id, "tags"),
+                                                     (note_id, "moreTags")])
 
 
 def test_add_note_with_existing_tags(conn, note_db):
-    note_id = note_db.add_note(conn, "stuff2", "things", ["tags", "moreTags"])
+    note_id = note_db.add_note("stuff2", "things", ["tags", "moreTags"])
 
-    print(conn.execute("SELECT note_id FROM note NATURAL join (tagmap NATURAL join tag) WHERE note_id = ?", (note_id,)))
-
-    assert (conn.execute("""SELECT note_id FROM note WHERE note_id = ?;""", (note_id, ))
-            .fetchone() == (note_id, ))
-    assert (conn.execute("""SELECT name FROM tag""").fetchall() == [
-        ("tags", ), ("moreTags", )
-    ])
-
-    tag_ids = map(lambda x: x[0],
-                  conn.execute("""SELECT tag_id FROM tag""").fetchall())
-    tagmap = list(map(lambda x: (note_id, x), tag_ids))
-    assert (conn.execute(
-        """SELECT note_id, tag_id FROM tagmap WHERE note_id = ?""",
-        (note_id, )).fetchall() == tagmap)
+    assert (conn.execute("""SELECT note.note_id, tag.name FROM note
+        JOIN (tagmap JOIN tag ON tagmap.tag_id = tag.tag_id)
+        ON note.note_id = tagmap.note_id
+        WHERE note.note_id = ?""",
+                         (note_id, )).fetchall() == [(note_id, "tags"),
+                                                     (note_id, "moreTags")])
 
 
 def test_delete_note(conn, note_db):
-    note_db.delete_note(conn, 2)
+    note_db.delete_note(2)
     assert (conn.execute("SELECT note_id FROM note WHERE note_id = ?",
                          (2, )).fetchone() is None)
     assert (conn.execute("SELECT note_id FROM tagmap WHERE note_id = ?",
                          (2, )).fetchall() == [])
 
 
-def test_update_note(conn, note_db):
-    note_id = 1
-    note_db.update_note(conn, note_id, "not stuff", "not things",
-                        ["tags", "lessTags"])
-    # note = conn.execute(
-    #     "SELECT title, text, date_modified FROM note WHERE id = ?",
-    #     (note_id, )).fetchone()
-    # tagmap = conn.execute("SELECT title, text, date_modified, name")
+# def test_update_note(conn, note_db):
+#     note_id = 1
+#     note_db.update_note(note_id, "not stuff", "not things",
+#                         ["tags", "lessTags"])
+#     assert (conn.execute(
+#         """SELECT note.note_id, title, text, tag.name FROM note
+#         JOIN (tagmap JOIN tag ON tagmap.tag_id = tag.tag_id)
+#         ON note.note_id = tagmap.note_id
+#         WHERE note.note_id = ?""",
+#         (note_id, )).fetchall() == [(note_id, "not stuff", "not things",
+#                                      "tags"), (note_id, "not stuff",
+#                                                "not things", "lessTags")])
